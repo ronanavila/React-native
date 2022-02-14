6f971858d564ac1174df10038c568d37433dee73
Ronan Avila <avila.ronan@gmail.com>
2022-02-14 13:55:24 -0300

    WIP

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 import { Input } from "../../components/Forms/Input";
 import { Button } from "../../components/Forms/Button";
+import { TransactionTypeButton } from "../../components/Forms/TransactionTypeButton";
+import { CategorySelect } from "../../components/Forms/CategorySelect";
 import { 
     Container,
     Header,
     Title,
     Form,
-    Fields
+    Fields,
+    TransactionTypes
  } from "./styles";
 
 
 export function Register(){
+    const [transactionType, setTransactionType] = useState('');
+
+    function handleTransactionTypeSelect(type: 'up' | 'down'){
+        setTransactionType(type);
+    }
+
     return(
         <Container>
             <Header>
@@ -19,8 +28,26 @@ export function Register(){
             </Header>
             <Form>
                 <Fields>
+
                     <Input placeholder="Nome"/>
                     <Input placeholder="Preço"/>
+                    <TransactionTypes>
+                        <TransactionTypeButton
+                            type="up"
+                            title="Income"
+                            onPress={() => handleTransactionTypeSelect('up')}
+                            isActive={transactionType === 'up'}
+                        />
+
+                        <TransactionTypeButton
+                            type="down"
+                            title="Outcome"
+                            onPress={() => handleTransactionTypeSelect('down')}
+                            isActive={transactionType === 'down'}
+                        />
+                    </TransactionTypes>
+                    <CategorySelect title="Categoria"/>
+                   
                 </Fields>
                 <Button title="Enviar"/>
             </Form>
